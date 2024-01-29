9785615151d365ecfd29af0c4eb822013b4911fd
dibborah <dibyajyotiborah469@gmail.com>
2024-01-29 13:36:23 +0530

    isSubmitting in-built loading state in reactHookForm and making onSubmit an Async function

--- src/App.js
+++ src/App.js
@@ -4,36 +4,68 @@ const App = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
+    setError,
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    try{
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      throw new Error();
+      // eslint-disable-next-line no-unreachable
+      console.log(data);      
+    }catch(error){
+      setError("root",{
+        message:"This email is already taken"
+      })
+
+    }
   };
 
   return (
-    <form className="flex flex-col m-3 p-5 bg-slate-950" onSubmit={handleSubmit(onSubmit)}>
+    <form
+      className="flex flex-col m-3 p-5 bg-slate-950"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <input
         className="border border-red-500 m-1"
         {...register("email", {
           required: "Email is required",
-          validate: (value) => value.includes("@"),
+          validate: (value) => {
+            if (!value.includes("@")) {
+              return "Email must include @ symbol";
+            }
+            return true;
+          },
         })}
         type="text"
         placeholder="Email"
       />
-      {errors.email && <div className="text-red-500">{errors.email.message}</div>}
+      {errors.email && (
+        <div className="text-red-500">{errors.email.message}</div>
+      )}
       <input
         className="border border-red-500 m-1"
         {...register("password", {
           required: "Password is required",
-          minLength: 8,
+          minLength: {
+            value: 8,
+            message: "Password must have atleast 8 characters",
+          },
         })}
         type="password"
         placeholder="Password"
       />
-      {errors.password && <div className="text-red-500">{errors.password.message}</div>}
-      <button className="border border-red-500 m-1 text-white " type="submit">Submit</button>
+      {errors.password && (
+        <div className="text-red-500">{errors.password.message}</div>
+      )}
+      
+      <button className="border border-red-500 m-1 text-white " type="submit">
+        {isSubmitting ? "...Loading" : "Submitted"}
+      </button>
+      {errors.root && (
+        <div className="text-red-500">{errors.root.message}</div>
+      )}
     </form>
   );
 };
